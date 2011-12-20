774c54beb7bb5c8608b1b776a816c87bbd575069
Denton Gentry <dgentry@google.com>
2011-12-20 10:49:29 -0800

    squashfs support for installer.
    ginstall.py: automatically set NVRAM variables to boot from the newly installed image
    ginstall.py: add --loader option, to install a bootloader.
    ginstall.py: allow --partition without an image file, to set NVRAM variables
    hnvram: Add support for new ACTIVATED_KERNEL_NAME and EXTRA_KERNEL_OPT variables.
    buildimage: move vmlinuz creation to buildroot
    buildimage: bundle the Squashfs filesystem into the image, not UBIFS.
    
    Change-Id: Ife844ab87a87ef751ebcfb7f22bedf922e239d62

--- bruno/ginstall/ginstall.py
+++ bruno/ginstall/ginstall.py
@@ -15,11 +15,12 @@ import tarfile
 
 
 # unit tests can override these with fake versions
-FLASH_ERASE = "/usr/sbin/flash_erase"
-PROC_MTD = "/proc/mtd"
-MTDBLOCK = "/dev/mtdblock{0}"
-UBIFORMAT = "/usr/sbin/ubiformat"
 BUFSIZE = 256 * 1024
+FLASH_ERASE = "/usr/sbin/flash_erase"
+HNVRAM = "/usr/bin/hnvram"
+MTDBLOCK = "/dev/mtdblock{0}"
+PROC_MTD = "/proc/mtd"
+UBIFORMAT = "/usr/sbin/ubiformat"
 
 # Verbosity of output
 quiet = False
@@ -28,6 +29,16 @@ def verbose_print(string):
   if not quiet:
     sys.stdout.write(string)
     sys.stdout.flush()
+
+
+def set_boot_partition(partition):
+  extra = "ubi.mtd=rootfs{0} root=mtdblock:rootfs rootfstype=squashfs".format(partition)
+  cmd = [HNVRAM,
+         '-w', 'MTD_TYPE_FOR_KERNEL=RAW',
+         '-w', 'ACTIVATED_KERNEL_NAME=kernel{0}'.format(partition),
+         '-w', 'EXTRA_KERNEL_OPT={0}'.format(extra)]
+  devnull = open('/dev/null', 'w')
+  return subprocess.call(cmd, stdout=devnull, stderr=devnull)
 
 
 def get_mtd_num(arg):
@@ -55,10 +66,37 @@ def get_erase_size(mtd):
   splt = re.compile('[ :]+')
   f = open(PROC_MTD)
   for line in f:
-    fields = splt.split(line)
-    if len(fields) >= 2 and fields[0].strip() == mtd:
+    fields = splt.split(line.strip())
+    if len(fields) >= 2 and fields[0] == mtd:
       return int(fields[2], 16)
   return 0
+
+
+def get_mtd_dev_for_partition(name):
+  """Find the mtd# for a named partition.
+
+  In /proc/mtd we have:
+
+  dev:    size   erasesize  name
+  mtd0: 00200000 00010000 "cfe"
+  mtd1: 00200000 00010000 "reserve0"
+  mtd2: 10000000 00100000 "kernel0"
+  mtd3: 10000000 00100000 "kernel1"
+
+  Args:
+    name: the partition to find. For example, "kernel0"
+
+  Returns:
+    The mtd device, for example "mtd2"
+  """
+  splt = re.compile('[ :]+')
+  quotedname = '"' + name + '"'
+  f = open(PROC_MTD)
+  for line in f:
+    fields = splt.split(line.strip())
+    if len(fields) >= 4 and fields[3] == quotedname:
+      return fields[0]
+  return None
 
 
 def round_to(orig, mult):
@@ -159,10 +197,16 @@ class FileImage(object):
     self.rootfsfile = rootfsfile
 
   def GetKernel(self):
-    return open(self.kernelfile, "rb")
+    if self.kernelfile:
+      return open(self.kernelfile, "rb")
+    else:
+      return None
 
   def GetRootFs(self):
-    return open(self.rootfsfile, "rb")
+    if self.rootfsfile:
+      return open(self.rootfsfile, "rb")
+    else:
+      return None
 
 
 class TarImage(object):
@@ -173,10 +217,10 @@ class TarImage(object):
 
   def GetKernel(self):
     try:
-      return self.tar_f.extractfile("vmlinuz.ubi")
+      return self.tar_f.extractfile("vmlinuz")
     except KeyError:
       try:
-        return self.tar_f.extractfile("vmlinux.ubi")
+        return self.tar_f.extractfile("vmlinux")
       except KeyError:
         return None
 
@@ -184,14 +228,10 @@ class TarImage(object):
     try:
       return self.tar_f.extractfile("rootfs.squashfs_ubi")
     except KeyError:
-      try:
-        return self.tar_f.extractfile("rootfs.ubi")
-      except KeyError:
-        return None
+      return None
 
 
-gfhd100_partitions = {"primary": ("mtd3", "mtd5"),
-                      "secondary": ("mtd4", "mtd6")}
+gfhd100_partitions = {"primary": 0, "secondary": 1}
 
 def main():
   parser = optparse.OptionParser()
@@ -204,6 +244,9 @@ def main():
   parser.add_option('-r', '--rootfs', dest='rootfsfile',
                     help='rootfs UBI image to install',
                     default=None)
+  parser.add_option('--loader', dest='loaderfile',
+                    help='bootloader to install',
+                    default=None)
   parser.add_option('-p', '--partition', dest='partition', metavar="PART",
                     type="string", action="store",
                     help='primary or secondary image partition',
@@ -214,41 +257,51 @@ def main():
 
   (options, args) = parser.parse_args()
   quiet = options.quiet
-  if options.tarfile:
-    img = TarImage(options.tarfile)
-    if options.kernfile or options.rootfsfile:
-      print("--tar option provided, ignoring --kernel and --rootfs")
-  else:
-    img = FileImage(options.kernfile, options.rootfsfile)
-  if not options.partition:
-    print("A --partition option must be provided.")
-    return 1
-  if options.partition not in gfhd100_partitions:
-    print("--partition must be one of: " + str(gfhd100_partitions.keys()))
+  if options.loaderfile is not None:
+    print("DO NOT INTERRUPT OR POWER CYCLE, or you will brick the unit.");
+    try:
+      loader = open(options.loaderfile, "rb")
+    except IOError:
+      print("unable to open loader file %s" % options.loaderfile)
+      return 1
+    mtd = get_mtd_dev_for_partition("cfe")
+    verbose_print("Writing loader to {0}".format(mtd))
+    install_to_mtd(loader, mtd)
 
-  mtds = gfhd100_partitions[options.partition]
-  kern = img.GetKernel()
-  if kern:
-    verbose_print("Writing kernel to {0}".format(mtds[0]))
-    install_to_ubi(kern, mtds[0])
-    verbose_print("\n")
+  if options.tarfile or options.kernfile or options.rootfsfile:
+    if not options.partition:
+      print("A --partition option must be provided.")
+      return 1
+    if options.partition not in gfhd100_partitions:
+      print("--partition must be one of: " + str(gfhd100_partitions.keys()))
 
-  rootfs = img.GetRootFs()
-  if rootfs:
-    verbose_print("Writing rootfs to {0}".format(mtds[1]))
-    install_to_ubi(rootfs, mtds[1])
-    verbose_print("\n")
+    if options.tarfile:
+      img = TarImage(options.tarfile)
+      if options.kernfile or options.rootfsfile:
+        print("--tar option provided, ignoring --kernel and --rootfs")
+    else:
+      img = FileImage(options.kernfile, options.rootfsfile)
 
-  # TODO(dgentry) set NVRAM variables automatically
-  print("To use this SW image, variables must be manually set in the bootloader.")
-  print('setenv -p BOOT "FLASH"')
-  print('setenv -p MTD_TYPE_FOR_KERNEL "mtd"')
-  if options.partition == "primary":
-    print('setenv -p ACTIVATED_KERNEL_NUM 0')
-    print('setenv -p KERNEL_OPT "ubi.mtd=5 root=ubi0:rootfs rootfstype=ubifs" (plus any ip= options)')
-  else:
-    print('setenv -p ACTIVATED_KERNEL_NUM 1')
-    print('setenv -p KERNEL_OPT "ubi.mtd=6 root=ubi0:rootfs rootfstype=ubifs" (plus any ip= options)')
+    pnum = gfhd100_partitions[options.partition]
+    kern = img.GetKernel()
+    if kern is not None:
+      mtd = get_mtd_dev_for_partition("kernel" + str(pnum))
+      verbose_print("Writing kernel to {0}".format(mtd))
+      install_to_mtd(kern, mtd)
+      verbose_print("\n")
+
+    rootfs = img.GetRootFs()
+    if rootfs is not None:
+      mtd = get_mtd_dev_for_partition("rootfs" + str(pnum))
+      verbose_print("Writing rootfs to {0}".format(mtd))
+      install_to_ubi(rootfs, mtd)
+      verbose_print("\n")
+
+  if options.partition:
+    pnum = gfhd100_partitions[options.partition]
+    verbose_print("Setting boot partition to kernel{0}\n".format(pnum))
+    set_boot_partition(pnum)
+
   return 0
 
 
