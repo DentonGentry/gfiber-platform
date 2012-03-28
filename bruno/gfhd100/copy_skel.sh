9189d9f65e15743edf27b8be7d4eb887252f4bb0
Ke Dong <kedong@google.com>
2012-03-28 16:20:53 -0700

    Remove -d -t options from builder.py.
    Consolidate the options under -r (production).
    The console and dropbear will be disabled by default although the
    software is installed as part of the image. To enabled them, there
    are two ways:
    - In EXTRA_KERNEL_OPT, add "debug=y".
    - If using NFS as rootfs, simply touch the file /tmp/DEBUG in the
    rootfs.
    
    Change-Id: Ibd54f56e6b94308c8ae523e6c7a5f2c1e6077a78

--- bruno/gfhd100/copy_skel.sh
+++ bruno/gfhd100/copy_skel.sh
@@ -3,6 +3,3 @@ set -x
 TARGET_DIR=$1
 
 cp -alf ${TARGET_DIR}/../build/bruno-HEAD/bruno/gfhd100/skel/. ${TARGET_DIR}/
-if (grep "\-test" ${TARGET_DIR}/etc/version); then
-  perl -p -i -e "s%^# TEST_AGETTY%S0:1:respawn:/sbin/agetty -L ttyS0 115200 vt100%" ${TARGET_DIR}/etc/inittab
-fi
