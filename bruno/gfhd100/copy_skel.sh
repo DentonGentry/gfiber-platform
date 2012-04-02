498b2d816dbb6ca83e852270d37c48cef45726f8
Avery Pennarun <apenwarr@gmail.com>
2012-04-02 00:45:50 -0400

    copy_skel: use hardlinks instead of rsync.
    This runs fast, plus if you edit a file in $O/target/, it'll automatically
    update it here.
    
    Change-Id: I7df3cd1fe88579dfe830b9e1ec4449278ba71d16

--- bruno/gfhd100/copy_skel.sh
+++ bruno/gfhd100/copy_skel.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-
+set -x
 TARGET_DIR=$1
 
-rsync -aP ${TARGET_DIR}/../build/bruno-HEAD/bruno/gfhd100/skel/ ${TARGET_DIR}/
+cp -alf ${TARGET_DIR}/../build/bruno-HEAD/bruno/gfhd100/skel/. ${TARGET_DIR}/
 if (grep "\-test" ${TARGET_DIR}/etc/version); then
   perl -p -i -e "s%^# TEST_AGETTY%S0:1:respawn:/sbin/agetty -L ttyS0 115200 vt100%" ${TARGET_DIR}/etc/inittab
 fi
--- bruno/gfhd100/copy_skel_initramfs.sh
+++ bruno/gfhd100/copy_skel_initramfs.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-
+set -x
 TARGET_DIR=$1
 CWD=$(dirname $0)
 ${CWD}/copy_skel.sh ${TARGET_DIR}
-install -m 0755 ${TARGET_DIR}/../build/bruno-HEAD/bruno/gfhd100/init.ramfs ${TARGET_DIR}/init
+ln -f ${TARGET_DIR}/../build/bruno-HEAD/bruno/gfhd100/init.ramfs ${TARGET_DIR}/init
+chmod 0755 ${TARGET_DIR}/init
