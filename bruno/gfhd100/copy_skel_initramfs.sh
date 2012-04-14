a07d71a4f7b04be625be1b38a5cc3046b0a3638b
Avery Pennarun <apenwarr@gmail.com>
2012-04-13 20:19:44 -0400

    platform/bruno changes for simpleramfs.
    These changes are needed in order to work with the simpleramfs changes in
    buildroot.  Since simpleramfs has its own startup script, all the stuff in
    /etc/init.d now needs to run, not just the stuff that used to run in the
    initramfs.  Also, the feature of S04volcheck that used to mount root can go
    away; now we only mount the non-root volumes.  And we can remove some
    scripts that are no longer called.
    
    Change-Id: I38de447f79fa75dad500bba5fff9fb3ddf9ee0ac

--- bruno/gfhd100/copy_skel_initramfs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -x
-TARGET_DIR=$1
-CWD=$(dirname $0)
-${CWD}/copy_skel.sh ${TARGET_DIR}
-ln -f ${TARGET_DIR}/../build/bruno-HEAD/bruno/gfhd100/init.ramfs ${TARGET_DIR}/init
-chmod 0755 ${TARGET_DIR}/init
