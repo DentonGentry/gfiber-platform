94f3201f570240264da9b2ead52a06098ac31cf5
Irina Stanescu <irinams@google.com>
2013-01-23 18:21:38 -0800

    Added notifications for file creation/deletion.
    Also, I removed some debug printfs and refactored the code a bit.
    
    Change-Id: I70582646a1a9733f0a86798e2ee498eb48beb8e9

--- cmds/dir-monitor.cc
+++ cmds/dir-monitor.cc
@@ -28,7 +28,6 @@ static int is_dir(const char *dir_path)
   if (S_ISDIR(sb.st_mode))
     return 1;
 
-  cerr << dir_path << " is not a directory" << endl;
   return 0;
 }
 
@@ -56,7 +55,6 @@ string DirMonitor::GetDirPath(int dir_wd) const
 
 int DirMonitor::GetWatchDescriptor(const string &dir_path) const
 {
-  cout << "Trying to find the watch descriptor for " << dir_path << endl;
   const auto it = p2wd_map_.find(dir_path);
   if (it == p2wd_map_.end()) {
     cerr << "No watch descriptor found for " << dir_path
@@ -75,16 +73,12 @@ int DirMonitor::GetParentWatchDescriptor(int dir_wd) const
 
 int DirMonitor::GetParentWatchDescriptor(const string &path, int dir_wd) const
 {
-  cout << "Trying to find parent watch descriptor for "
-       << dir_wd << " and " << path << endl;
-
   size_t pos = path.find_last_of("/");
   // If there is no "/" in the path`
   if (pos == string::npos) {
     cerr << path << " has no parent " << endl;
     return -1;
   }
-  cout << "Parent path " << path.substr(0, pos) << endl;
   return GetWatchDescriptor(path.substr(0, pos));
 }
 
@@ -92,23 +86,17 @@ void DirMonitor::StorePair(const string &path, int wd)
 {
   p2wd_map_[path] = wd;
   wd2p_map_[wd] = path;
-  cout << "Will add to both maps pair (" << wd << ", " << path << ")" << endl;
 }
-
 
 void DirMonitor::RemovePair(const string &path, int wd)
 {
   p2wd_map_.erase(path);
   wd2p_map_.erase(wd);
-  cout << "Will remove from both maps pair (" << wd
-       << ", " << path << ")" << endl;
 }
-
 
 void DirMonitor::AddToParentList(int pwd, int wd)
 {
   if (pwd == -1) return;
-  cout << "Will add " << wd << " to the list of " << pwd << endl;
   wds_[pwd].insert(wd);
 }
 
@@ -122,7 +110,6 @@ void DirMonitor::RemoveFromParentList(const string &path, int wd)
 void DirMonitor::RemoveFromParentList(int pwd, int wd)
 {
   if (pwd == -1) return;
-  cout << "Will remove " << wd << " from the list of " << pwd << endl;
   wds_[pwd].erase(wd);
 }
 
@@ -143,7 +130,7 @@ DirMonitor::DirMonitor(int argc, char *argv[]) : inotify_fd_(-1)
 
     // Remove all the trailing "/" from the path
     path.erase(path.find_last_not_of("/") + 1);
-    cout << "New path " << path << endl;
+    cout << "Adding new watch for path " << path << endl;
 
     if (AddWatchRecursively(path, -1) < 0) {
       cerr << "adding watch recursively for " << path << " failed" << endl;
@@ -152,17 +139,16 @@ DirMonitor::DirMonitor(int argc, char *argv[]) : inotify_fd_(-1)
   }
 }
 
-int DirMonitor::AddWatch(const string &dir_path, int pwd)
+int DirMonitor::AddWatch(const string &path, int pwd, int events)
 {
-  int dir_wd = inotify_add_watch(inotify_fd_, dir_path.c_str(),
-                                 INOTIFY_EVENTS | IN_ONLYDIR);
-  if (dir_wd < 0) {
+  int wd = inotify_add_watch(inotify_fd_, path.c_str(), events);
+  if (wd < 0) {
     perror("inotify_add_watch failed");
     return -1;
   }
-  StorePair(dir_path, dir_wd);
-  AddToParentList(pwd, dir_wd);
-  return dir_wd;
+  StorePair(path, wd);
+  AddToParentList(pwd, wd);
+  return wd;
 }
 
 int DirMonitor::AddWatchRecursively(const string &dir_path, int pwd)
@@ -176,7 +162,7 @@ int DirMonitor::AddWatchRecursively(const string &dir_path, int pwd)
     return -1;
   }
 
-  cerr << "Trying to add directory: " << dir_path << endl;
+  cout << "Add watch for directory: " << dir_path << endl;
 
   fd = open(dir_path.c_str(), O_NOFOLLOW | O_RDONLY);
   if (fd < 0) {
@@ -198,7 +184,7 @@ int DirMonitor::AddWatchRecursively(const string &dir_path, int pwd)
     close(fd);
     return -1;
   }
-  dir_wd = AddWatch(dir_path, pwd);
+  dir_wd = AddWatch(dir_path, pwd, INOTIFY_DIR_EVENTS);
 
   while (dir_entry) {
     // Skipping . and ..
@@ -213,9 +199,12 @@ int DirMonitor::AddWatchRecursively(const string &dir_path, int pwd)
     full_path.append("/");
     full_path.append(dir_entry->d_name);
 
-    if (is_dir(full_path.c_str()) &&
-        AddWatchRecursively(full_path, dir_wd) < 0) {
-      cerr << "Add failed for " << full_path << endl;
+    if (is_dir(full_path.c_str())) {
+      if (AddWatchRecursively(full_path, dir_wd) < 0) {
+        cerr << "Add failed for " << full_path << endl;
+      }
+    } else {
+        AddWatch(full_path, dir_wd, INOTIFY_FILE_EVENTS);
     }
     dir_entry = readdir(dir);
   }
@@ -226,73 +215,31 @@ int DirMonitor::AddWatchRecursively(const string &dir_path, int pwd)
   return 1;
 }
 
-/*
-//TODO(irinams): remove this code if it turns out not to be useful
-//any more
-int DirMonitor::RemoveWatch(const string &dir_path, int dir_wd)
-{
-  if (dir_path.empty()) {
-    cerr << "Invalid dir path for remove watch" << endl;
-    return -1;
-  }
-
-  cout << "Will remove directory " << dir_path
-       << " and watch descriptor " << dir_wd << endl;
-
-  if (inotify_rm_watch(inotify_fd_, dir_wd) < 0) {
-    perror("inotify_rm_watch failed");
-    return -1;
-  }
-//TODO(irinams): remove this code if it turns out not to be useful
-//any more
-  RemoveFromParentList(dir_wd);
-  RemovePair(dir_path, dir_wd);
-  return 1;
-}
-*/
-
-/*
-//TODO(irinams): remove this code if it turns out not to be useful
-//any more
-int DirMonitor::RemoveWatchRecursively(int dir_wd)
-{
-  cerr << "Will remove watch_descriptor " << dir_wd << endl;
-  for (set<int>::iterator it = wds_[dir_wd].begin();
-       it != wds_[dir_wd].end(); ++it) {
-    if (RemoveWatchRecursively(*it) < 0) {
-      cerr << "RemoveWatchRecursively failed for wd " << *it << endl;
-    }
-  }
-
-  string dir_path = GetDirPath(dir_wd, &dir_path);
-  if (dir_path.empty()) return -1;
-
-  if (RemoveWatch(dir_path, dir_wd) < 0) {
-    perror("inotify_rm_watch failed");
-    return -1;
-  }
-
-  wds_[dir_wd].clear();
-  return 1;
-}
-*/
-
-
 // Handler for the IN_CREATE event:
-// File/directory created in watched directory (*).
-void DirMonitor::HandleCreate(const char *name, int pwd)
+// Directory created in watched directory (*).
+void DirMonitor::HandleDirectoryCreate(const char *name, int pwd)
 {
-  cout << "directory " << name << " triggered IN_CREATE for parent wd "
+  cout << "IN_CREATE triggered for directory " << name << " and parent wd "
        << pwd << endl;
 
   string full_path = GetFullDirPath(name, pwd);
   if (full_path.empty()) return;
 
   if (AddWatchRecursively(full_path, pwd) < 0) {
-    cerr << " watch recursively for " << full_path << " failed" << endl;
+    cerr << "Add watch recursively for " << full_path << " failed" << endl;
   }
 }
 
+// Handler for the IN_CREATE event:
+// File created in watched directory (*).
+void DirMonitor::HandleFileCreate(const char *name, int pwd)
+{
+  cout << "IN_CREATE triggered for file " << name << " and parent wd "
+       << pwd << endl;
+  string full_path = GetFullDirPath(name, pwd);
+  if (full_path.empty()) return;
+  AddWatch(full_path, pwd, INOTIFY_FILE_EVENTS);
+}
 
 // Handler for the IN_DELETE_SELF inotify event:
 // Watched file/directory was itself deleted.
@@ -301,45 +248,16 @@ void DirMonitor::HandleDeleteSelf(int wd)
   cout << "IN_DELETE_SELF triggered for wd " << wd << endl;
   string path = GetDirPath(wd);
   if (path.empty()) return;
-  cout << "\tdirectory " << path << endl;
-
   RemovePair(path, wd);
   wds_[wd].clear();
-
   RemoveFromParentList(path, wd);
-}
-
-
-// Handler for the IN_DELETE inotify event:
-// File/directory deleted from watched directory (*).
-void DirMonitor::HandleDelete(const char *name, int wd)
-{
-  cerr << "directory " << name << " triggered IN_DELETE for parent wd "
-       << wd << endl;
-
-  string full_path = GetFullDirPath(name, wd);
-  if (full_path.empty()) return;
-
-  int dir_wd = GetWatchDescriptor(full_path);
-  if (dir_wd == -1) return;
-
-  /*
-  //TODO(irinams): remove this code
-  // If a folder has been deleted, the watch was automatically removed too
-  if (RemoveWatch(full_path, dir_wd) < 0) {
-    cerr << "RemoveWatch for " << full_path << " and "
-      << dir_wd << " failed " << endl;
-  }
-  */
-  RemovePair(full_path, dir_wd);
-  RemoveFromParentList(wd, dir_wd);
 }
 
 // Handler for the IN_MOVED_FROM event:
 // File moved out of watched directory (*).
 void DirMonitor::HandleMoveFrom(const char *name, int wd)
 {
-  cerr << "directory " << name << " triggered IN_MOVED_FROM for parent wd "
+  cout << "IN_MOVED_FROM triggered for directory " << name << " and parent wd "
        << wd << endl;
 
   string full_path = GetFullDirPath(name, wd);
@@ -348,15 +266,6 @@ void DirMonitor::HandleMoveFrom(const char *name, int wd)
   int dir_wd = GetWatchDescriptor(full_path);
   if (dir_wd == -1) return;
 
-  /*
-  //TODO(irinams): remove this code
-  // If a folder has been deleted, the watch was automatically removed too
-  // and all it's subdirectories
-  if (RemoveWatch(full_path, dir_wd) < 0) {
-    cerr << "RemoveWatch for " << full_path << " and "
-      << dir_wd << " failed " << endl;
-  }
-  */
   RemovePair(full_path, dir_wd);
   RemoveFromParentList(wd, dir_wd);
 }
@@ -365,7 +274,7 @@ void DirMonitor::HandleMoveFrom(const char *name, int wd)
 // File moved into watched directory (*).
 void DirMonitor::HandleMoveTo(const char *name, int wd)
 {
-  cerr << "directory " << name << " triggered IN_MOVED_TO for parent wd "
+  cout << "IN_MOVED_TO triggered for directory " << name << " and parent wd "
        << wd << endl;
 
   string full_path = GetFullDirPath(name, wd);
@@ -397,9 +306,9 @@ void DirMonitor::StartMonitoring()
     }
 
     for (ptr = buf; ptr < buf + len; ptr += event->len + sizeof(*event)) {
-
       event = (struct inotify_event *)ptr;
-      // Check to see if the the event struct is not incomplete.
+
+      // Check to see if the the event struct is complete.
       if (ptr + sizeof(*event) > buf + len) {
         cerr << "inotify: incomplete inotify event" << endl;
         break;
@@ -409,34 +318,34 @@ void DirMonitor::StartMonitoring()
         //     << " for parent watch descriptor " << event->wd << endl;
         continue;
       } else if (event->mask & IN_Q_OVERFLOW) {
-        cerr << "inotify: event queue overflowed" << endl;
+        cout << "Event queue overflowed" << endl;
         break;
       }
 
-      cout << "EVENT WD: " << event->wd << endl;
-      cout << "EVENT MASK: " << event->mask << endl;
-      cout << "EVENT COOKIE: " << event->cookie << endl;
-      cout << "EVENT LEN: " << event->len << endl;
+      //cout << "EVENT WD: " << event->wd << endl;
+      //cout << "EVENT MASK: " << event->mask << endl;
+      //cout << "EVENT COOKIE: " << event->cookie << endl;
+      //cout << "EVENT LEN: " << event->len << endl;
 
       if (event->mask & IN_DELETE_SELF) {
         HandleDeleteSelf(event->wd);
       }
-      if (event->mask & IN_CREATE) {
-        HandleCreate(event->name, event->wd);
+
+      if (event->mask & IN_ISDIR) {
+        if (event->mask & IN_CREATE) {
+          HandleDirectoryCreate(event->name, event->wd);
+        }
+        if (event->mask & IN_MOVED_FROM) {
+          HandleMoveFrom(event->name, event->wd);
+        }
+        if (event->mask & IN_MOVED_TO) {
+          HandleMoveTo(event->name, event->wd);
+        }
+      } else {
+        if (event->mask & IN_CREATE) {
+          HandleFileCreate(event->name, event->wd);
+        }
       }
-      //if (event->mask & IN_DELETE) {
-      //  HandleDelete(event->name, event->wd);
-      //}
-      if (event->mask & IN_MOVED_FROM) {
-        HandleMoveFrom(event->name, event->wd);
-      }
-      if (event->mask & IN_MOVED_TO) {
-        HandleMoveTo(event->name, event->wd);
-      }
-      //if (event->len && ptr + sizeof(*event) + event->len <= buf + len) {
-      // Pathname is null terminated.
-      //  cout << "***********" << event->name << endl;
-      //}
     }
   }
 
@@ -453,6 +362,7 @@ int main(int argc, char *argv[])
     cerr << "usage: " << argv[0] << " <dirname_1>...<dirname_n>" << endl;
     exit(1);
   }
+
   DirMonitor monitor(argc - 1, argv + 1);
   monitor.StartMonitoring();
   return 0;
