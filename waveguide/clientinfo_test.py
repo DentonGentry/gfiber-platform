efc76546c65833703da461756900ac3ff12df25d
Jonathan Rockway <jrockway@chromium.org>
2015-08-17 20:19:37 -0400

    Fix lint error.
    Change-Id: I158c7877286340de7f64636a03e6489cd8dc3a60

--- waveguide/clientinfo_test.py
+++ waveguide/clientinfo_test.py
@@ -12,11 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# pylint:disable=invalid-name
 
 import taxonomy
-import clientinfo  # pylint: g-import-not-at-top
+import clientinfo
 from wvtest import wvtest
 
 
