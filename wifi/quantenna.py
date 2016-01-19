DentonGentry/gfiber-platform

commit e78c51a6b0f45b5ee1b1a6b73a56165c91f77f61

/bin/wifi: use get-quantenna-interface script.
Also add a unit test (and collateral damage).

Change-Id: Iad27d13458d9a2f5daf357a2f60035f3e6efed30

wifi/quantenna.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python -S
 
 """Wifi commands for Quantenna using QCSAPI."""
 
@@ -9,27 +9,31 @@ import time
 import utils
 
 
+def _get_interface():
+  return subprocess.check_output(['get-quantenna-interface']).strip() or None
+
+
 def _get_qcsapi():
-  """Detect Quantenna device."""
-  if not hasattr(_get_qcsapi, 'qcsapi'):
-    _get_qcsapi.qcsapi = None
-    if (utils.subprocess_quiet(['runnable', 'qcsapi_pcie_static']) == 0 and
-        utils.subprocess_quiet(['modinfo', 'qdpc-host'], no_stdout=True) == 0):
-      # qcsapi_pcie_static runs on PCIe hosts, e.g. GFRG250. qdpc-host is only
-      # loaded if a Quantenna device is present.
-      _get_qcsapi.qcsapi = 'qcsapi_pcie_static'
-    elif utils.subprocess_quiet(['runnable', 'call_qcsapi']) == 0:
-      # call_qcsapi runs on the LHOST, e.g. GFEX250.
-      _get_qcsapi.qcsapi = 'call_qcsapi'
-  return _get_qcsapi.qcsapi
+  # qcsapi_pcie_static runs on PCIe hosts, e.g. GFRG250.
+  # call_qcsapi runs on the LHOST, e.g. GFEX250.
+  return next((qcsapi for qcsapi in ['qcsapi_pcie_static', 'call_qcsapi']
+               if utils.subprocess_quiet(['runnable', qcsapi]) == 0), None)
+
+
+def _get_mac_address():
+  var = {'wlan0': 'MAC_ADDR_WIFI', 'wlan1': 'MAC_ADDR_WIFI2'}[_get_interface()]
+  return subprocess.check_output(['hnvram', '-rq', var]).strip()
 
 
 def _qcsapi(*args):
-  return subprocess.check_output([_get_qcsapi()] + list(args))
+  return subprocess.check_output([_get_qcsapi()] + list(args)).strip()
 
 
 def _set(mode, opt):
   """Enable wifi."""
+  if opt.band != '5' or not _get_interface() or not _get_qcsapi():
+    return False
+
   _qcsapi('rfenable', '0')
   _qcsapi('restore_default_config', 'noreboot')
 
@@ -43,12 +47,7 @@ def _set(mode, opt):
   for param, value in config.iteritems():
     _qcsapi('update_config_param', 'wifi0', param, value)
 
-  macs = {'wlan0': 'MAC_ADDR_WIFI', 'wlan1': 'MAC_ADDR_WIFI2'}
-  for dev, var in macs.iteritems():
-    if utils.read_or_empty('/sys/class/net/%s/device/vendor' % dev) == '0x1bb5':
-      mac = subprocess.check_output(['hnvram', '-rq', var]).strip()
-      _qcsapi('set_mac_addr', 'wifi0', mac)
-      break
+  _qcsapi('set_mac_addr', 'wifi0', _get_mac_address())
 
   if int(_qcsapi('is_startprod_done')):
     _qcsapi('reload_in_mode', 'wifi0', mode)
@@ -67,7 +66,7 @@ def _set(mode, opt):
     _qcsapi('set_option', 'wifi0', 'ssid_broadcast',
             '0' if opt.hidden_mode else '1')
     _qcsapi('rfenable', '1')
-  else:
+  elif mode == 'sta':
     _qcsapi('create_ssid', 'wifi0', opt.ssid)
     _qcsapi('ssid_set_passphrase', 'wifi0', opt.ssid, '0',
             os.environ['WIFI_CLIENT_PSK'])
@@ -78,14 +77,13 @@ def _set(mode, opt):
   return True
 
 
-def _stop(_):
+def _stop(opt):
   """Disable wifi."""
+  if opt.band != '5' or not _get_interface() or not _get_qcsapi():
+    return False
+
   _qcsapi('rfenable', '0')
   return True
-
-
-def has_quantenna():
-  return _get_qcsapi() is not None
 
 
 def set_wifi(opt):

wifi/wifi.py
@@ -220,8 +220,8 @@ def set_wifi(opt):
   if opt.encryption == 'WEP' or '_PSK_' in opt.encryption:
     psk = os.environ['WIFI_PSK']
 
-  if band == '5' and quantenna.has_quantenna():
-    return quantenna.set_wifi(opt)
+  if quantenna.set_wifi(opt):
+    return True
 
   if iw.RUNNABLE_WL() and not iw.RUNNABLE_IW():
     _set_wifi_broadcom(opt)
@@ -354,8 +354,8 @@ def stop_ap_wifi(opt):
   for band in opt.band.split():
     utils.log('stopping AP for %s GHz...', band)
 
-    if band == '5' and quantenna.has_quantenna():
-      success &= quantenna.stop_ap_wifi(opt)
+    if quantenna.stop_ap_wifi(opt):
+      success &= True
       continue
 
     interface = iw.find_interface_from_band(
@@ -813,8 +813,8 @@ def set_client_wifi(opt):
 
   psk = os.environ.get('WIFI_CLIENT_PSK', None)
 
-  if band == '5' and quantenna.has_quantenna():
-    return quantenna.set_client_wifi(opt)
+  if quantenna.set_client_wifi(opt):
+    return True
 
   phy = iw.find_phy(band, 'auto')
   if phy is None:
@@ -866,8 +866,8 @@ def stop_client_wifi(opt):
   for band in opt.band.split():
     utils.log('stopping client for %s GHz...', band)
 
-    if band == '5' and quantenna.has_quantenna():
-      success &= quantenna.stop_client_wifi(opt)
+    if quantenna.stop_client_wifi(opt):
+      success &= True
       continue
 
     interface = iw.find_interface_from_band(

wifi/quantenna_test.py
@@ -1,0 +1,162 @@
+#!/usr/bin/python -S
+
+"""Tests for quantenna.py."""
+
+import os
+from configs_test import FakeOptDict
+import quantenna
+from wvtest import wvtest
+
+
+calls = []
+
+
+def fake_qcsapi(*args):
+  calls.append(list(args))
+  if args[0] == 'is_startprod_done':
+    return '1\n' if ['startprod', 'wifi0'] in calls else '0\n'
+
+
+def set_fakes(interface='wlan1', qcsapi='qcsapi_pcie_static'):
+  del calls[:]
+  os.environ['WIFI_PSK'] = 'wifi_psk'
+  os.environ['WIFI_CLIENT_PSK'] = 'wifi_client_psk'
+  quantenna._get_interface = lambda: interface
+  quantenna._get_qcsapi = lambda: qcsapi
+  quantenna._get_mac_address = lambda: '00:11:22:33:44:55'
+  quantenna._qcsapi = fake_qcsapi
+
+
+def matching_calls_indices(accept):
+  return [i for i, c in enumerate(calls) if c[0] in accept]
+
+
+@wvtest.wvtest
+def not_5ghz_test():
+  opt = FakeOptDict()
+  opt.band = '2.4'
+  set_fakes()
+  wvtest.WVFAIL(quantenna.set_wifi(opt))
+  wvtest.WVFAIL(quantenna.set_client_wifi(opt))
+  wvtest.WVFAIL(quantenna.stop_ap_wifi(opt))
+  wvtest.WVFAIL(quantenna.stop_client_wifi(opt))
+  wvtest.WVPASSEQ(calls, [])
+
+
+@wvtest.wvtest
+def not_quantenna_test():
+  opt = FakeOptDict()
+  opt.band = '5'
+  set_fakes(interface='')
+  wvtest.WVFAIL(quantenna.set_wifi(opt))
+  wvtest.WVFAIL(quantenna.set_client_wifi(opt))
+  wvtest.WVFAIL(quantenna.stop_ap_wifi(opt))
+  wvtest.WVFAIL(quantenna.stop_client_wifi(opt))
+  wvtest.WVPASSEQ(calls, [])
+  set_fakes(qcsapi='')
+  wvtest.WVFAIL(quantenna.set_wifi(opt))
+  wvtest.WVFAIL(quantenna.set_client_wifi(opt))
+  wvtest.WVFAIL(quantenna.stop_ap_wifi(opt))
+  wvtest.WVFAIL(quantenna.stop_client_wifi(opt))
+  wvtest.WVPASSEQ(calls, [])
+  set_fakes(interface='', qcsapi='')
+  wvtest.WVFAIL(quantenna.set_wifi(opt))
+  wvtest.WVFAIL(quantenna.set_client_wifi(opt))
+  wvtest.WVFAIL(quantenna.stop_ap_wifi(opt))
+  wvtest.WVFAIL(quantenna.stop_client_wifi(opt))
+  wvtest.WVPASSEQ(calls, [])
+
+
+@wvtest.wvtest
+def set_wifi_test():
+  opt = FakeOptDict()
+  opt.band = '5'
+  set_fakes()
+
+  # Run set_wifi for the first time.
+  wvtest.WVPASS(quantenna.set_wifi(opt))
+
+  # 'rfenable 0' must be run first so that a live interface is not being
+  # modified.
+  wvtest.WVPASSEQ(calls[0], ['rfenable', '0'])
+
+  # 'restore_default_config noreboot' must be run before any configuration so
+  # that old configuration is cleared.
+  wvtest.WVPASSEQ(calls[1], ['restore_default_config', 'noreboot'])
+
+  # Check that 'reload_in_mode' is not run.
+  wvtest.WVPASS(['reload_in_mode', 'wifi0'] not in calls)
+
+  # Check that configs are written.
+  wvtest.WVPASS(['update_config_param', 'wifi0', 'bw', '20'] in calls)
+  wvtest.WVPASS(['update_config_param', 'wifi0', 'channel', '149'] in calls)
+  wvtest.WVPASS(['update_config_param', 'wifi0', 'mode', 'ap'] in calls)
+  wvtest.WVPASS(['update_config_param', 'wifi0', 'pmf', '0'] in calls)
+  wvtest.WVPASS(['update_config_param', 'wifi0', 'scs', '0'] in calls)
+  wvtest.WVPASS(['set_mac_addr', 'wifi0', '00:11:22:33:44:55'] in calls)
+  wvtest.WVPASS(['set_ssid', 'wifi0', 'TEST_SSID'] in calls)
+  wvtest.WVPASS(['set_passphrase', 'wifi0', '0', 'wifi_psk'] in calls)
+  wvtest.WVPASS(['set_option', 'wifi0', 'ssid_broadcast', '1'] in calls)
+
+  # 'update_config_param' and 'set_mac_addr' must be run before 'startprod',
+  # since 'startprod' runs scripts that read these configs.
+  sp = calls.index(['startprod', 'wifi0'])
+  i = matching_calls_indices(['update_config_param', 'set_mac_addr'])
+  wvtest.WVPASSLT(i[-1], sp)
+
+  # 'startprod' must be followed by 'is_startprod_done'.
+  wvtest.WVPASSEQ(calls[sp + 1], ['is_startprod_done'])
+
+  # Other configs must be written after 'startprod' and before 'rfenable 1'.
+  i = matching_calls_indices(['set_ssid', 'set_passphrase', 'set_option'])
+  wvtest.WVPASSLT(sp, i[0])
+  wvtest.WVPASSLT(i[-1], calls.index(['rfenable', '1']))
+
+  # Run set_wifi again in client mode with new options.
+  opt.channel = '147'
+  opt.ssid = 'TEST_SSID2'
+  opt.width = '80'
+  new_calls_start = len(calls)
+  wvtest.WVPASS(quantenna.set_client_wifi(opt))
+
+  # Clear old calls.
+  del calls[:new_calls_start]
+
+  # 'rfenable 0' must be run first so that a live interface is not being
+  # modified.
+  wvtest.WVPASSEQ(calls[0], ['rfenable', '0'])
+
+  # 'restore_default_config noreboot' must be run before any configuration so
+  # that old configuration is cleared.
+  wvtest.WVPASSEQ(calls[1], ['restore_default_config', 'noreboot'])
+
+  # Check that 'startprod' is not run.
+  wvtest.WVPASS(['startprod', 'wifi0'] not in calls)
+
+  # Check that configs are written.
+  wvtest.WVPASS(['update_config_param', 'wifi0', 'bw', '80'] in calls)
+  wvtest.WVPASS(['update_config_param', 'wifi0', 'channel', '147'] in calls)
+  wvtest.WVPASS(['update_config_param', 'wifi0', 'mode', 'sta'] in calls)
+  wvtest.WVPASS(['update_config_param', 'wifi0', 'pmf', '0'] in calls)
+  wvtest.WVPASS(['update_config_param', 'wifi0', 'scs', '0'] in calls)
+  wvtest.WVPASS(['set_mac_addr', 'wifi0', '00:11:22:33:44:55'] in calls)
+  wvtest.WVPASS(['create_ssid', 'wifi0', 'TEST_SSID2'] in calls)
+  wvtest.WVPASS(['ssid_set_passphrase', 'wifi0', 'TEST_SSID2', '0',
+                 'wifi_client_psk'] in calls)
+
+  # 'update_config_param' and 'set_mac_addr' must be run before
+  # 'reload_in_mode', since 'reload_in_mode' runs scripts that read these
+  # configs.
+  rim = calls.index(['reload_in_mode', 'wifi0', 'sta'])
+  i = matching_calls_indices(['update_config_param', 'set_mac_addr'])
+  wvtest.WVPASSLT(i[-1], rim)
+
+  # Other configs must be written after 'reload_in_mode' and before
+  # 'apply_security_config'.
+  i = matching_calls_indices(['create_ssid', 'ssid_set_passphrase'])
+  wvtest.WVPASSLT(rim, i[0])
+  wvtest.WVPASSLT(i[-1], calls.index(['apply_security_config', 'wifi0']))
+
+
+if __name__ == '__main__':
+  wvtest.wvtest_main()

commit 3be1ea30b8535d503e5fae4791905d2c4b7c6b75

/bin/wifi: fix opt.band handling for Quantenna.
Commit e78c51a6b0f45b5ee1b1a6b73a56165c91f77f61 introduced a bug in
quantenna._stop where passing opt.band = '2.4 5' results in a false
error.

Change-Id: Ia412f5e4e07e50db6b65a3dfb0f50bd2dc16bbef

wifi/quantenna.py
@@ -31,7 +31,7 @@ def _qcsapi(*args):
 
 def _set(mode, opt):
   """Enable wifi."""
-  if opt.band != '5' or not _get_interface() or not _get_qcsapi():
+  if not _get_interface() or not _get_qcsapi():
     return False
 
   _qcsapi('rfenable', '0')
@@ -77,9 +77,9 @@ def _set(mode, opt):
   return True
 
 
-def _stop(opt):
+def _stop(_):
   """Disable wifi."""
-  if opt.band != '5' or not _get_interface() or not _get_qcsapi():
+  if not _get_interface() or not _get_qcsapi():
     return False
 
   _qcsapi('rfenable', '0')

wifi/wifi.py
@@ -220,7 +220,7 @@ def set_wifi(opt):
   if opt.encryption == 'WEP' or '_PSK_' in opt.encryption:
     psk = os.environ['WIFI_PSK']
 
-  if quantenna.set_wifi(opt):
+  if band == '5' and  quantenna.set_wifi(opt):
     return True
 
   if iw.RUNNABLE_WL() and not iw.RUNNABLE_IW():
@@ -354,7 +354,7 @@ def stop_ap_wifi(opt):
   for band in opt.band.split():
     utils.log('stopping AP for %s GHz...', band)
 
-    if quantenna.stop_ap_wifi(opt):
+    if band == '5' and quantenna.stop_ap_wifi(opt):
       success &= True
       continue
 
@@ -813,7 +813,7 @@ def set_client_wifi(opt):
 
   psk = os.environ.get('WIFI_CLIENT_PSK', None)
 
-  if quantenna.set_client_wifi(opt):
+  if band == '5' and quantenna.set_client_wifi(opt):
     return True
 
   phy = iw.find_phy(band, 'auto')
@@ -866,7 +866,7 @@ def stop_client_wifi(opt):
   for band in opt.band.split():
     utils.log('stopping client for %s GHz...', band)
 
-    if quantenna.stop_client_wifi(opt):
+    if band == '5' and quantenna.stop_client_wifi(opt):
       success &= True
       continue
 

wifi/quantenna_test.py
@@ -32,21 +32,8 @@ def matching_calls_indices(accept):
 
 
 @wvtest.wvtest
-def not_5ghz_test():
-  opt = FakeOptDict()
-  opt.band = '2.4'
-  set_fakes()
-  wvtest.WVFAIL(quantenna.set_wifi(opt))
-  wvtest.WVFAIL(quantenna.set_client_wifi(opt))
-  wvtest.WVFAIL(quantenna.stop_ap_wifi(opt))
-  wvtest.WVFAIL(quantenna.stop_client_wifi(opt))
-  wvtest.WVPASSEQ(calls, [])
-
-
-@wvtest.wvtest
 def not_quantenna_test():
   opt = FakeOptDict()
-  opt.band = '5'
   set_fakes(interface='')
   wvtest.WVFAIL(quantenna.set_wifi(opt))
   wvtest.WVFAIL(quantenna.set_client_wifi(opt))
@@ -70,7 +57,6 @@ def not_quantenna_test():
 @wvtest.wvtest
 def set_wifi_test():
   opt = FakeOptDict()
-  opt.band = '5'
   set_fakes()
 
   # Run set_wifi for the first time.
@@ -156,6 +142,17 @@ def set_wifi_test():
   i = matching_calls_indices(['create_ssid', 'ssid_set_passphrase'])
   wvtest.WVPASSLT(rim, i[0])
   wvtest.WVPASSLT(i[-1], calls.index(['apply_security_config', 'wifi0']))
+
+
+@wvtest.wvtest
+def stop_wifi_test():
+  opt = FakeOptDict()
+  set_fakes()
+  wvtest.WVPASS(quantenna.stop_ap_wifi(opt))
+  wvtest.WVPASS(['rfenable', '0'] in calls)
+  set_fakes()
+  wvtest.WVPASS(quantenna.stop_client_wifi(opt))
+  wvtest.WVPASS(['rfenable', '0'] in calls)
 
 
 if __name__ == '__main__':