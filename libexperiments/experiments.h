0cdd7487a58947e73e35fa090f478899d3c7f48a
Chris Kuiper <ckuiper@google.com>
2016-03-08 11:59:37 -0800

    platform: add libexperiments
    This provides a library for C/C++ programs that enables them to easily
    register experiments and check if they are enabled.
    
    In order to accommodate programs like Sagesrv which may be checking
    experiment states at higher frequencies, a caching logic is used that
    stores the current state of registered experiments and updates them in
    (configurable) intervals (e.g., Sagesrv defaults to 60 secs).
    
    Please see experiments.h for details on usage.
    
    Change-Id: I2d55161960dc5010beb666324666f6fd1f210f97

---
+++ libexperiments/experiments.h
@@ -0,0 +1,217 @@
+#ifndef _LIBEXPERIMENTS_EXPERIMENTS_H
+#define _LIBEXPERIMENTS_EXPERIMENTS_H
+
+#include <inttypes.h>
+
+
+// Implements a library that supports the Gfiber Experiments framework, as
+// explained in the following doc: go/gfiber-experiments-framework.
+//
+// Both C and C++ (class) implementations are available.
+//
+// C++ example:
+// ====================================
+//   const char* kConfigFolderPath[] = "/config/experiments";
+//   int64_t kMinTimeBetweenRefreshUs = 60 * 1000 * 1000;  // 60 secs
+//   e = new Experiments();
+//   if (!e->Initialize(kConfigFolderPath, kMinTimeBetweenRefreshUs,
+//                      {"exp1", "exp2"})) {
+//     // handle error case
+//   }
+//
+//   // later in the code
+//   if (e->IsEnabled("exp1")) {
+//     // exp1 is enabled
+//     [..]
+//   }
+//
+// C example:
+// ===================================
+//   const char* kConfigFolderPath[] = "/config/experiments";
+//   int64_t kMinTimeBetweenRefreshUs = 60 * 1000 * 1000;  // 60 secs
+//   if (!experiments_initialize(kConfigFolderPath, kMinTimeBetweenRefreshUs,
+//                               NULL);  // use default register function
+//     // handle error case
+//   }
+//
+//   experiments_register("exp1");
+//   experiments_register("exp2");
+//
+//   // later in the code
+//   if (experiments_is_enabled("exp1")) {
+//     // exp1 is enabled
+//     [..]
+//   }
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Function called when registering a new experiment.
+// Returns non-zero (boolean true) for success, else 0 (boolean false).
+typedef int (*experiments_register_func_t) (const char *name);
+
+// Default experiment register function. Calls the shell script
+// "register_experiment <name>".
+int DefaultExperimentsRegisterFunc(const char *name);
+
+// Dummy experiment register function. Just returns true.
+int DummyExperimentsRegisterFunc(const char *name);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+//
+// C++ implementation
+//
+#ifdef __cplusplus
+
+#include <atomic>
+#include <mutex>  // NOLINT
+#include <set>
+#include <string>
+#include <vector>
+
+
+class Experiments {
+ public:
+  Experiments()
+      : initialized_(false),
+        min_time_between_refresh_usec_(0),
+        last_time_refreshed_usec_(0) {}
+  virtual ~Experiments() {}
+
+  // Initializes the instance:
+  // * Sets the provided experiments config directory and register function.
+  // * Calls the register function for the provided experiment names.
+  // * Scans the config folder to determine initial state of all registered
+  //   experiments.
+  // The min_time_between_refresh_usec values sets a lower boundary on how
+  // often the config folder is scanned for updated experiment states.
+  // Returns true if successful.
+  bool Initialize(const std::string &config_dir,
+                  int64_t min_time_between_refresh_usec,
+                  experiments_register_func_t register_func,
+                  const std::vector<std::string> &names_to_register);
+  // Convenience version, using default experiments register function.
+  bool Initialize(const std::string &config_dir,
+                  int64_t min_time_between_refresh_usec,
+                  const std::vector<std::string> &names_to_register) {
+    return Initialize(config_dir, min_time_between_refresh_usec,
+                      &DefaultExperimentsRegisterFunc, names_to_register);
+  }
+
+  bool IsInitialized() const { return initialized_; }
+
+  // Registers the provided experiment(s).
+  bool Register(const std::vector<std::string> &names);
+  bool Register(const std::string &name) {
+    std::vector<std::string> names{name};
+    return Register(names);
+  }
+
+  // Returns true if the given experiment is registered.
+  bool IsRegistered(const std::string &name);
+
+  // Returns true if the given experiment is active, else false. If the minimum
+  // time between refreshes has passed, re-scans the config folder for updates
+  // first.
+  bool IsEnabled(const std::string &name);
+
+ private:
+  // Registers the given experiments. Unlocked version takes lock_ first.
+  // Returns true if successful, else false.
+  bool Register_Unlocked(const std::vector<std::string> &names);
+  bool Register_Locked(const std::vector<std::string> &names);
+
+  // Returns true if the given experiment is in the list of registered
+  // experiments.
+  bool IsInRegisteredList(const std::string &name) const {
+    return registered_experiments_.find(name) != registered_experiments_.end();
+  }
+
+  // Refreshes all registered experiment states by scanning the config folder.
+  void Refresh();
+
+  // Updates the state of the given experiment by checking its file in the
+  // config folder.
+  void UpdateState(const std::string &name);
+
+  // Returns true if the given experiment is in the list of enabled
+  // experiments.
+  bool IsInEnabledList(const std::string &name) {
+    return enabled_experiments_.find(name) != enabled_experiments_.end();
+  }
+
+  std::atomic<bool> initialized_;
+  std::mutex lock_;
+
+  // Experiments config folder, containing the system-wide list of experiments.
+  // An experiment is marked active if the folder contains the file named
+  // "<experiment_name>.active".
+  std::string config_dir_;
+
+  // External function called to register an experiment.
+  experiments_register_func_t register_func_;
+
+  std::set<std::string> registered_experiments_;
+  std::set<std::string> enabled_experiments_;
+
+  // Minimum time between accessing the config folder to refresh the experiment
+  // states. When set to 0 it refreshes on every call to IsEnabled().
+  uint64_t min_time_between_refresh_usec_;
+  uint64_t last_time_refreshed_usec_;
+};
+
+extern Experiments *experiments;
+
+#endif  // __cplusplus
+
+
+//
+// C-based API
+//
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Creates and initializes the experiments object:
+// * Sets the provided experiments config directory and register function.
+// * Calls the register function for the provided experiment names.
+// * Scans the config folder to determine initial state of all registered
+//   experiments.
+// The min_time_between_refresh_usec values sets a lower boundary on how often
+// the config folder is scanned for updated experiment states. Set
+// register_func to NULL to use the default register function
+// (DefaultExperimentsRegisterFunc()).
+// Returns non-zero (boolean true) if successful, 0 (boolean false) for error.
+int experiments_initialize(const char *config_dir,
+                            int64_t min_time_between_refresh_usec,
+                            experiments_register_func_t register_func);
+
+// Returns non-zero (boolean true) if the experiments object is initialized,
+// else 0 (boolean false).
+int experiments_is_initialized();
+
+// Registers the provided experiment.
+// Returns non-zero (boolean true) if successful, 0 (boolean false) for error.
+int experiments_register(const char *name);
+
+// Returns non-zero (boolean true) if the given experiment name is registered,
+// else 0 (boolean false).
+int experiments_is_registered(const char *name);
+
+// Returns non-zero (boolean true) if the given experiment is active, else 0
+// (boolean false). If the minimum time between refreshes has passed, re-scans
+// the config folder for updates first.
+int experiments_is_enabled(const char *name);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // _LIBEXPERIMENTS_EXPERIMENTS_H
---
+++ libexperiments/utils.h
@@ -0,0 +1,83 @@
+#ifndef _LIBEXPERIMENTS_UTILS_H_
+#define _LIBEXPERIMENTS_UTILS_H_
+
+#include <stdint.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <string>
+#include <ostream>
+#include <vector>
+
+//
+// Subset of utils functions copied from vendor/google/mcastcapture/utils/.
+//
+
+// Namespace is needed to avoid conflicts when other modules with identical
+// function names are linking against libexperiments.so.
+namespace libexperiments_utils {
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+
+const int64_t kUsecsPerSec = 1000000LL;
+const int64_t kNsecsPerSec = 1000000000LL;
+const int64_t kNsecsPerUsec = 1000LL;
+
+static inline int64_t secs_to_usecs(int64_t secs) {
+  return secs * kUsecsPerSec;
+}
+
+void log(const char* cstr, ...)
+    __attribute__((format(printf, 1, 2)));
+
+void log_perror(int err, const char* cstr, ...)
+    __attribute__((format(printf, 2, 3)));
+
+// Measures elapsed time in usecs.
+uint64_t us_elapse(uint64_t start_time_us);
+
+void us_sleep(uint64_t usecs);
+
+static inline bool file_exists(const char *name) {
+  return access(name, F_OK) == 0;
+}
+
+static inline bool directory_exists(const char *path) {
+  struct stat dir_stat;
+  if (stat(path, &dir_stat) != 0) {
+    return false;
+  }
+  return S_ISDIR(dir_stat.st_mode);
+}
+
+// This function runs the command cmd (but not in a shell), providing the
+// return code, stdout, and stderr. It also allows to specify the stdin,
+// and a command timeout value (timeout_usec, use <0 to block indefinitely).
+// Note that the timeout is fired only if the command stops producing
+// either stdout or stderr. A process that periodically produces output
+// will never be killed.
+// Returns an extended error code:
+// - 0 if everything is successful,
+//  -1 if any step fails,
+//  -2 if timeout, and
+//  -3 if the command output (stdout+stderr) is too large (kMaxRunCmdOutput).
+int run_cmd(const std::vector<std::string> &cmd, const std::string &in,
+            int *status,
+            std::ostream *out,
+            std::ostream *err,
+            int64_t timeout_usec);
+
+//
+// String printf functions, ported from stringprintf.cc/h
+//
+
+// Return a C++ string
+std::string StringPrintf(const char* format, ...)
+    __attribute__((format(printf, 1, 2)));
+
+}  // namespace libexperiments_utils
+
+#endif  // _LIBEXPERIMENTS_UTILS_H_
