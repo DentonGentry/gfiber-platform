e858c06c120fa4ef26a9da18b1e501e3a628aac8
Chris Kuiper <ckuiper@google.com>
2017-03-06 17:03:36 -0800

    libexperiments: add missing includes
    Change-Id: Ia80966b92af54456452cc86aa7b528062170d367

--- libexperiments/utils.h
+++ libexperiments/utils.h
@@ -1,8 +1,10 @@
 #ifndef _LIBEXPERIMENTS_UTILS_H_
 #define _LIBEXPERIMENTS_UTILS_H_
 
+#include <errno.h>
 #include <fcntl.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
